ec6dc576aa1ceac6e3b3818030544b99a6452b7c
Andrey Ermolaev <122530648+enOnesohC@users.noreply.github.com>
2023-08-26 22:50:16 +0300

    тесты по категориям

---
+++ Playhop/categories/test_category_two_players.py
@@ -0,0 +1,55 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+import time
+
+
+#не получается использовать из-за окна после перехода на главную страницу
+#метод быстро находит кнопку и кликает по ней, но она ещё недоступна
+#так что будут дебильные временные задержки
+#browser.implicitly_wait(5)
+
+#two playes
+
+
+def test_category_two_players():
+
+    #Это, чтобы язык браузера установить на англ (почему то русский язык по умолчанию
+    #открывается через selenium, хотя вроде убрал рус яз из настроек)
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option('prefs', {'intl.accept_languages': 'en,en_US'})
+    browser = webdriver.Chrome(options=options)
+
+
+    #Адрес сайта, который открываем
+    link = "https://playhop.com/"
+    browser.get(link)
+    time.sleep(3)
+
+    # находим кнопку и нажимаем
+    # и что, всегда надо нажимать эту кнопку??
+    button = browser.find_element(By.XPATH,"//*[text()='Consent']")
+    button.click()
+    # задержка
+    time.sleep(2)
+
+    #клик по кнопке с категорией
+    button1 = browser.find_element(By.XPATH, "//*[text()='Two players']")
+
+    #этим скроллим до нужного элемента
+    #можно использовать submit()  вместо click()
+    #в таком случае кнопка нажмётся даже если не видна на странице
+    #но это вроде не надо
+    button1.location_once_scrolled_into_view
+
+    button1.click()
+    time.sleep(2)
+
+    #проверка что действительно открылась страница c нужным текстом в заголовке
+    title = browser.find_element(By.CLASS_NAME, "section-header__text")
+    assert "Two players" in title.text, "Переход не выполнен, либо заголовок не найден"
+    time.sleep(3)
+
+    browser.quit()
+
+#test_category_two_players()
